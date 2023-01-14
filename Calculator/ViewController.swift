e8bd62f63bf1f669134e1d21d8c5494044c72379
hasandgursoy <hasan.d.gursoy@gmail.com>
2023-01-14 17:04:49 +0300

    Calculator App : My First Swfit App

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstNumber: UITextField!
+    
+    @IBOutlet weak var secondNumber: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func sumButton(_ sender: Any) {
+        
+        if let firstNo = Int(firstNumber.text ?? "0") {
+            if let secondNo = Int(secondNumber.text ?? "0") {
+                resultLabel.text = String(firstNo + secondNo);
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func minusButton(_ sender: Any) {
+        if let firstNo = Int(firstNumber.text ?? "0") {
+            if let secondNo = Int(secondNumber.text ?? "0") {
+                resultLabel.text = String(firstNo - secondNo);
+            }
+        }
+    }
+    
+    
+    @IBAction func multipleButton(_ sender: Any) {
+        if let firstNo = Int(firstNumber.text ?? "0") {
+            if let secondNo = Int(secondNumber.text ?? "0") {
+                resultLabel.text = String(firstNo * secondNo);
+            }
+        }
+    }
+
+    @IBAction func divideButton(_ sender: Any) {
+        if let firstNo = Int(firstNumber.text ?? "0") {
+            if let secondNo = Int(secondNumber.text ?? "0") {
+                resultLabel.text = String(firstNo / secondNo);
+            }
+        }
+    }
+    
 }
 
